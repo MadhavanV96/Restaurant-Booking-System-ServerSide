6cac6e57a018d5d35f90b6795b3d412b1ae23074
MadhavanV96 <13isid4me@gmail.com>
2025-01-05 04:41:54 +0530

    cors first update

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ const app = express();
 
 
 
+const allowedOrigins = ['http://localhost:5173', 'https://restaurantbookingclient.netlify.app/']
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (e.g., Postman, cURL, same-origin requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error("Blocked by CORS: ", origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  })
+);
+
 
 app.use(cors());
 
